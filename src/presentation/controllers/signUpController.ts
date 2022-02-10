a22ddb51956af5b3620b7851b851ffa52df79a78
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-02-09 23:48:26 -0300

    refactor: signUpController dinamically checking for missing attributes

--- src/presentation/controllers/signUpController.ts
+++ src/presentation/controllers/signUpController.ts
@@ -9,11 +9,30 @@ export class SignUpController {
     }
 
     handle(httpRequest: IHttpRequest): IHttpResponse {
-        console.log(this.test, httpRequest)
-        if (!httpRequest.body.name) {
-            return badRequest(new MissingParamError('name'))
+        console.log(this.test)
+
+        const requiredAttributes = ['name', 'email', 'password']
+
+        const missingAttribute = requiredAttributes
+            .map((attribute) => {
+                if (!httpRequest.body[attribute]) {
+                    return badRequest(new MissingParamError(attribute))
+                }
+
+                return {
+                    statusCode: 200,
+                    body: {},
+                }
+            })
+            .find((attribute) => attribute.statusCode === 400)
+
+        if (missingAttribute) {
+            return missingAttribute
         }
 
-        return badRequest(new MissingParamError('email'))
+        return {
+            statusCode: 200,
+            body: {},
+        }
     }
 }
