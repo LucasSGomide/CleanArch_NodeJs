1904b2d2f9e6bff67a6c9984d3c2f097db21e50d
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-02-09 22:16:17 -0300

    feat: signUpController returns 400

---
+++ src/presentation/controllers/signUpController.ts
@@ -0,0 +1,12 @@
+export class SignUpController {
+    constructor(public test: string = 'Avoiding Eslint') {
+        this.test = test
+    }
+
+    handle(httpRequest: any): any {
+        console.log(this.test, httpRequest)
+        return {
+            statusCode: 400,
+        }
+    }
+}
