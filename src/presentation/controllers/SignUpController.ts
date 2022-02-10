7944cc8dfbe12ccd5760de29c9ee473dae4c8ab2
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-02-10 20:30:59 -0300

    feat: signUpController handles unexpected errors

--- src/presentation/controllers/SignUpController.ts
+++ src/presentation/controllers/SignUpController.ts
@@ -5,7 +5,8 @@ import { IEmailValidator } from '../protocols/IEmailValidator'
 
 import { MissingParamError } from '../errors/MissingParamError'
 import { InvalidParamError } from '../errors/InvalidParamError'
-import { badRequest } from '../helpers/HttpHelpers'
+import { ServerError } from '../errors/ServerError'
+import { badRequest, serverError } from '../helpers/HttpHelpers'
 
 export class SignUpController implements IController {
     private readonly emailValidator: IEmailValidator
@@ -15,34 +16,39 @@ export class SignUpController implements IController {
     }
 
     handle(httpRequest: IHttpRequest): IHttpResponse {
-        const requiredAttributes = [
-            'name',
-            'email',
-            'password',
-            'passwordConfirmation',
-        ]
+        try {
+            const requiredAttributes = [
+                'name',
+                'email',
+                'password',
+                'passwordConfirmation',
+            ]
 
-        const missingAttribute = requiredAttributes
-            .map((attribute) => {
-                if (!httpRequest.body[attribute]) {
-                    return badRequest(new MissingParamError(attribute))
-                }
+            const missingAttribute = requiredAttributes
+                .map((attribute) => {
+                    if (!httpRequest.body[attribute]) {
+                        return badRequest(new MissingParamError(attribute))
+                    }
 
-                return {
-                    statusCode: 200,
-                    body: {},
-                }
-            })
-            .find((attribute) => attribute.statusCode === 400)
+                    return {
+                        statusCode: 200,
+                        body: {},
+                    }
+                })
+                .find((attribute) => attribute.statusCode === 400)
 
-        if (missingAttribute) {
-            return missingAttribute
-        }
+            if (missingAttribute) {
+                return missingAttribute
+            }
 
-        const isEmailValid = this.emailValidator.isValid(httpRequest.body.email)
-
-        if (!isEmailValid) {
-            return badRequest(new InvalidParamError('email'))
+            const isEmailValid = this.emailValidator.isValid(
+                httpRequest.body.email
+            )
+            if (!isEmailValid) {
+                return badRequest(new InvalidParamError('email'))
+            }
+        } catch (error) {
+            return serverError(new ServerError())
         }
 
         return {
