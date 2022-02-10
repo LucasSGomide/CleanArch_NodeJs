9b8dedb8154c800ff7dccc5419642a29b89f3336
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-02-10 20:29:47 -0300

    feast: add server internal error

--- src/presentation/helpers/HttpHelpers.ts
+++ src/presentation/helpers/HttpHelpers.ts
@@ -4,3 +4,8 @@ export const badRequest = (error: Error): IHttpResponse => ({
     statusCode: 400,
     body: error,
 })
+
+export const serverError = (error: Error): IHttpResponse => ({
+    statusCode: 500,
+    body: error,
+})
