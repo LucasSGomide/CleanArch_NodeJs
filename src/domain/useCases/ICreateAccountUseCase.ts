113134e3a484793a379c0594c7bd41049eaf3a8e
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-02-10 22:51:15 -0300

    feat: add account model and createAccount useCase

---
+++ src/domain/useCases/ICreateAccountUseCase.ts
@@ -0,0 +1,11 @@
+import { AccountModel } from '../models/AccountModel'
+
+export type CreateAccountModel = {
+    name: string
+    email: string
+    password: string
+}
+
+export interface ICreateAccountUseCase {
+    create(account: CreateAccountModel): AccountModel
+}
