9bc7e7e9045b3587714518abf428a8924b96688f
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-02-11 22:12:08 -0300

    test: ensure EmailValidatorAdapter returns true if validator returns true

--- src/utils/__tests__/EmailValidatorAdapter.spec.ts
+++ src/utils/__tests__/EmailValidatorAdapter.spec.ts
@@ -4,8 +4,16 @@ describe('EmailValidatorAdapter', () => {
     test('Should return false if validator returns false', () => {
         const sut = new EmailValidatorAdapter()
 
-        const isValid = sut.isValid('invalida_email@mail.com')
+        const isValid = sut.isValid('invalid_email')
 
         expect(isValid).toBe(false)
+    })
+
+    test('Should return true if validator returns true', () => {
+        const sut = new EmailValidatorAdapter()
+
+        const isValid = sut.isValid('valid_email@mail.com')
+
+        expect(isValid).toBe(true)
     })
 })
