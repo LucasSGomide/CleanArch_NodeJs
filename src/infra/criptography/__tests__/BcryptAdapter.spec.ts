c7d43f5793056a0882d187138a88f1a05ccdec34
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-02-19 13:27:50 -0300

    refactor: move BcryptAdapter to a factory function

--- src/infra/criptography/__tests__/BcryptAdapter.spec.ts
+++ src/infra/criptography/__tests__/BcryptAdapter.spec.ts
@@ -1,16 +1,19 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from '../BcryptAdapter'
 
+const saltRounds = 12
+
 jest.mock('bcrypt', () => ({
     async hash(): Promise<string> {
         return Promise.resolve('hashed_value')
     },
 }))
 
+const makeSut = (): BcryptAdapter => new BcryptAdapter(saltRounds)
+
 describe('BcryptAdapter', () => {
     test('Should call Bcrypt with correct value', async () => {
-        const saltRounds = 12
-        const sut = new BcryptAdapter(saltRounds)
+        const sut = makeSut()
         const bcryptSpy = jest.spyOn(bcrypt, 'hash')
 
         await sut.encrypt('any_value')
@@ -19,8 +22,15 @@ describe('BcryptAdapter', () => {
     })
 
     test('Should return hashed value on success', async () => {
-        const saltRounds = 12
-        const sut = new BcryptAdapter(saltRounds)
+        const sut = makeSut()
+
+        const hashedValue = await sut.encrypt('any_value')
+
+        expect(hashedValue).toBe('hashed_value')
+    })
+
+    test('Should throw value on success', async () => {
+        const sut = makeSut()
 
         const hashedValue = await sut.encrypt('any_value')
 
