2adcffa42f160e6c6ceddf8ab8d9ba1edbbf8b19
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-02-19 10:38:34 -0300

    test: add mock to validator lib and ensure isValid method in EmailValidatorAdapter returns the correct values

--- src/utils/__tests__/EmailValidatorAdapter.spec.ts
+++ src/utils/__tests__/EmailValidatorAdapter.spec.ts
@@ -1,10 +1,23 @@
+import validator from 'validator'
 import { EmailValidatorAdapter } from '../EmailValidatorAdapter'
 
 describe('EmailValidatorAdapter', () => {
+    jest.mock('validator', () => ({
+        isEmail(): boolean {
+            return true
+        },
+    }))
+
     test('Should return false if validator returns false', () => {
+        // O que importa para esse teste é saber que se a lib do validator retornar falso, o método isValid do EmailValidatorAdapter vai retornar falso também.
+        // Não importa como a lib valida as entradas, o que é importante é o retorno estar correto dentro do método do SUT.
+        // Não é a lib do validator que está sendo testada. O teste é da implementação da minha classe que recebe o retorno de uma lib já testada.
+
+        jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
+
         const sut = new EmailValidatorAdapter()
 
-        const isValid = sut.isValid('invalid_email')
+        const isValid = sut.isValid('invalid_email@mail.com')
 
         expect(isValid).toBe(false)
     })
