2a1d6f2ee037e94219632fbf7e48b0b8bc386f7a
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-02-19 13:24:15 -0300

    test: ensure BcryptAdapter returns the correct hashed value on success

--- src/infra/criptography/__tests__/BcryptAdapter.spec.ts
+++ src/infra/criptography/__tests__/BcryptAdapter.spec.ts
@@ -1,8 +1,14 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from '../BcryptAdapter'
 
+jest.mock('bcrypt', () => ({
+    async hash(): Promise<string> {
+        return Promise.resolve('hashed_value')
+    },
+}))
+
 describe('BcryptAdapter', () => {
-    test('Showld call BCrypt with correct value', async () => {
+    test('Should call Bcrypt with correct value', async () => {
         const saltRounds = 12
         const sut = new BcryptAdapter(saltRounds)
         const bcryptSpy = jest.spyOn(bcrypt, 'hash')
@@ -10,5 +16,14 @@ describe('BcryptAdapter', () => {
         await sut.encrypt('any_value')
 
         expect(bcryptSpy).toHaveBeenCalledWith('any_value', saltRounds)
+    })
+
+    test('Should return hashed value on success', async () => {
+        const saltRounds = 12
+        const sut = new BcryptAdapter(saltRounds)
+
+        const hashedValue = await sut.encrypt('any_value')
+
+        expect(hashedValue).toBe('hashed_value')
     })
 })
